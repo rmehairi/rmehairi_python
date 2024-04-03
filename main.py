bd956f0a5fa64e1c7b784cf915ea24fc4ed8dff5
rmehairi <160010862+rmehairi@users.noreply.github.com>
2024-04-03 16:24:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Testing")
+from EventBooking import *
+from datetime import date
+
+if __name__ == "__main__":
+    artwork1 = Artwork("Mona Lisa", "Leonardo da Vinci", date(1503, 1, 1), "Iconic portrait")
+    exhibition = Exhibition("Masterpieces of Renaissance", Location("Room 101", "Gallery"), 30)
+    exhibition.add_artwork(artwork1)
+    print(exhibition)
+
+    visitor1 = Visitor("V001", "Khalid", 30)
+    ticket = Ticket("Regular", 63, Event("Renaissance Art Exhibition", exhibition.location, 30, 80), visitor1)
+    visitor1.buy_ticket(ticket)
+    print(visitor1.ticket)
+    visitor1.ticket.print_receipt()
